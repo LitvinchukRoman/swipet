2dd69e460bbf32bab0ec5cd4d0b8b68885defb6f
DenisKoriavets <d.korjavets@ukr.net>
2026-05-26 22:27:13 +0300

    feat: initialize backend project structure
    - Add main application class for Spring Boot setup
    - Create initial application properties configuration
    - Set up Gradle build with necessary plugins and dependencies
    - Establish .gitignore and .gitattributes for project files
    - Include basic test class for application context loading

---
+++ backend/src/test/java/ua/edu/ukma/swipet/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.swipet.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
